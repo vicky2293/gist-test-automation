c74b3763c431a02de1eede9510ac82363a046cdb
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-08-25 21:13:31 +0200

    Include vitest and tsconfig

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['api/**/*.test.ts'],
+    reporters: ['default'],
+  },
+});
