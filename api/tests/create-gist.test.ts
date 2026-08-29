94752f4717b225e01576e07bef481d7d47d8b81d
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-08-29 12:46:22 +0200

    Update test description

--- api/tests/create-gist.test.ts
+++ api/tests/create-gist.test.ts
@@ -17,13 +17,13 @@ describe('POST /gists', () => {
     validateSchema('createGist.json', response.body);
   });
 
-  test('cannot create gist with personal access token - returns 403', async () => {
+  test('cannot create gist with personal access token - returns 401', async () => {
     const response = await gistApi.createGist('unauthenticated', requestBody);
     expect(response.status).toBe(401);
     expect(response.body.message).toBe('Requires authentication');
   });
 
-  test('cannot create gist without required body paramter - returns 403', async () => {
+  test('cannot create gist without required body paramter - returns 422', async () => {
     const invalidRequestBody = {
       description: 'Example of a gist',
       public: false,
