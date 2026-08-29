3b68803f939c6cbb7f6a725204ad856aa9ec9454
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-08-29 12:49:35 +0200

    Make delete test independent of id

--- api/tests/delete-gist.test.ts
+++ api/tests/delete-gist.test.ts
@@ -1,10 +1,22 @@
 import { describe, expect, test } from 'vitest';
 import { gistApi } from '../fixtures/gist.fixture.js';
+import { validateSchema } from '../utils/schema-validator.js';
 
 describe('DELETE /gists/{gistId}', () => {
   test('delete gist - returns 204', async () => {
-    const response = await gistApi.deleteGist('authenticated', 'ENTER_GIST_ID'); //Enter Gist id during runtime
-    expect(response.status).toBe(204);
+    const requestBody = {
+      description: 'Example of a gist',
+      public: false,
+      files: { 'README.md': { content: 'Hello World' } },
+    };
+
+    const create_response = await gistApi.createGist('authenticated', requestBody);
+    expect(create_response.status).toBe(201);
+    const gist_id = create_response.body.id;
+    validateSchema('createGist.json', create_response.body);
+
+    const delete_response = await gistApi.deleteGist('authenticated', gist_id);
+    expect(delete_response.status).toBe(204);
   });
 
   test('cannot delete gist with invalid gistId - returns 404', async () => {
